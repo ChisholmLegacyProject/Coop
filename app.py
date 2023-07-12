dbe82c1caf64970a6c6bd69fcfb58c500ace5e52
ChisholmLegacyProject <135271749+ChisholmLegacyProject@users.noreply.github.com>
2023-07-12 10:12:19 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,9 @@
 import streamlit as st
 import pandas as pd
-import folium
 
 def load_data(csv_url):
     data = pd.read_csv(csv_url)
     return data
-
-def create_map(data):
-    # Create a map centered around the mean coordinates
-    map_center = [data['Latitude'].mean(), data['Longitude'].mean()]
-    my_map = folium.Map(location=map_center, zoom_start=10)
-
-    # Add markers for each coordinate
-    for _, row in data.iterrows():
-        folium.Marker(
-            location=[row['Latitude'], row['Longitude']],
-            popup=row['Name']  # Change 'Name' to the column name containing the marker label
-        ).add_to(my_map)
-
-    return my_map
 
 def main():
     st.title("CSV Map Viewer")
@@ -32,10 +17,12 @@ def main():
 
     # Check if 'Latitude' and 'Longitude' columns exist
     if 'Latitude' in data.columns and 'Longitude' in data.columns:
-        my_map = create_map(data)
-        st.folium_chart(my_map)  # Display the map
+        # Display the map
+        st.map(data)
     else:
         st.error("The CSV file should contain 'Latitude' and 'Longitude' columns.")
 
 if __name__ == '__main__':
     main()
+
+
